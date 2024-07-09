b57de59bdd14a6ba61182aec90917c40cfdcca30
elim0050 <elim0050@student.monash.edu>
2024-07-09 17:46:37 +1000

    requester model

---
+++ server/app/models/requester.model.js
@@ -0,0 +1,28 @@
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+        {
+            requesterName: String,
+            requesterEmail: String,
+            requesterAffilation: String,
+            googleScholar: String,
+            dblp: String,
+            justification: String,
+            algorithmName: String,
+            authorName: String,
+            paperReference: String,
+            githubLink: String,
+            comments: String,
+        }
+    );
+
+
+    schema.method("toJSON", function() {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+
+    const Requester = mongoose.model("requester", schema);
+    return Requester;
+};
