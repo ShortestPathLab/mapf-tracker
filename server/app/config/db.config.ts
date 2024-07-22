df81e725ce3220f58bdb82db4bbd2a147d3559fe
Kevin <kzhe0012@student.monash.edu>
2024-07-23 04:39:32 +1000

    Hide secrets

--- server/app/config/db.config.ts
+++ server/app/config/db.config.ts
@@ -3,7 +3,5 @@ import { config } from "dotenv";
 config();
 
 export default {
-  url:
-    process.env.MONGO_DB_URI ||
-    "mongodb://dev:isfuhewyug372j4u@118.138.234.90:8987/test?directConnection=true&authSource=admin",
+  url: process.env.MONGO_DB_URI,
 };
