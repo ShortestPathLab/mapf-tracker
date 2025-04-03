74ce217f873a5975347164f39eabbc5cde845ba4
Kevin Zheng <kevin.zheng@monash.edu>
2025-04-03 04:59:17 +0000

    Refactor LostConnectionWarning component to handle multiple connection states and update version to 0.2.2

--- client/src/components/LostConnectionWarning.tsx
+++ client/src/components/LostConnectionWarning.tsx
@@ -1,40 +1,53 @@
 import { Snackbar, Typography } from "@mui/material";
 import { useBottomBar } from "App";
 import usePortal from "react-useportal";
-import { Dot } from "./Dot";
-import { useXs } from "./dialog/useSmallDisplay";
+import { paper } from "theme";
 import { useConnectivity } from "../hooks/useConnectivity";
+import { Dot } from "./Dot";
 
 export function LostConnectionWarning() {
-  const { disconnected } = useConnectivity();
+  const { disconnected, offline } = useConnectivity();
   const { Portal } = usePortal({ bindTo: document.body });
   const { enabled: bottomBar } = useBottomBar();
-  const xs = useXs();
   return (
     <Portal>
-      <Snackbar
-        open={disconnected}
-        anchorOrigin={{ vertical: xs ? "bottom" : "top", horizontal: "center" }}
-        ContentProps={{
-          sx: {
-            transition: (t) =>
-              `${t.transitions.create([
-                "translate",
-                "opacity",
-                "transform",
-              ])} !important`,
-            translate: bottomBar ? "0 -80px" : "0",
-            color: "text.primary",
-            bgcolor: "background.paper",
-            justifyContent: "center",
-          },
-        }}
-        message={
-          <Typography variant="body2">
-            <Dot sx={{ bgcolor: "warning.main" }} /> Offline
-          </Typography>
-        }
-      />
+      {[
+        {
+          value: disconnected && !offline,
+          label: "Connection lost",
+          color: "warning.main",
+        },
+        { value: offline, label: "Offline", color: "text.secondary" },
+      ].map(
+        ({ value, label, color }) =>
+          value && (
+            <Snackbar
+              key={label}
+              open={disconnected}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+              ContentProps={{
+                sx: {
+                  transition: (t) =>
+                    `${t.transitions.create([
+                      "translate",
+                      "opacity",
+                      "transform",
+                    ])} !important`,
+                  translate: bottomBar ? "0 -80px" : "0",
+                  color: "text.primary",
+                  bgcolor: "background.paper",
+                  justifyContent: "center",
+                  ...paper(0),
+                },
+              }}
+              message={
+                <Typography variant="body2">
+                  <Dot sx={{ bgcolor: color }} /> {label}
+                </Typography>
+              }
+            />
+          )
+      )}
     </Portal>
   );
 }
