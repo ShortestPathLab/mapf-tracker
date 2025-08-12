792d1d333e3985d4068fc17b1fd073be61cb1a45
Kevin Zheng <kevin.zheng@monash.edu>
2025-08-12 10:37:56 +0000

    refactor: Restructure About component layout for improved responsiveness

--- client/src/pages/docs/about/index.tsx
+++ client/src/pages/docs/about/index.tsx
@@ -5,6 +5,7 @@ import {
   ListItem,
   ListItemAvatar,
   ListItemText,
+  Stack,
   Typography,
 } from "@mui/material";
 import { Prose } from "layout";
@@ -12,31 +13,37 @@ import { filter } from "lodash";
 import Content from "./about.md";
 import { people } from "./people";
 import { ArticleLayout } from "layout/ArticleLayout";
+import { useSm } from "components/dialog/useSmallDisplay";
 
 export function About() {
+  const sm = useSm();
   return (
-    <>
-      <Prose>
+    <Stack direction={sm ? "column" : "row"} spacing={2}>
+      <Prose sx={{ flex: 1 }}>
         <Content />
       </Prose>
-      {["Advisors", "Developers"].map((group) => (
-        <Card sx={{ py: 1, px: 1 }} key={group}>
-          <Typography variant="h5" component="h4" sx={{ px: 2, pt: 2 }}>
-            {group}
-          </Typography>
-          <List>
-            {filter(people, { group }).map(({ name, avatar, affiliation }) => (
-              <ListItem key={name}>
-                <ListItemAvatar>
-                  <Avatar alt={name} src={avatar} />
-                </ListItemAvatar>
-                <ListItemText primary={`${name}`} secondary={affiliation} />
-              </ListItem>
-            ))}
-          </List>
-        </Card>
-      ))}
-    </>
+      <Stack sx={{ flex: 1, gap: 2 }}>
+        {["Advisors", "Developers"].map((group) => (
+          <Card sx={{ py: 1, px: 1 }} key={group}>
+            <Typography variant="h5" component="h4" sx={{ px: 2, pt: 2 }}>
+              {group}
+            </Typography>
+            <List>
+              {filter(people, { group }).map(
+                ({ name, avatar, affiliation }) => (
+                  <ListItem key={name}>
+                    <ListItemAvatar>
+                      <Avatar alt={name} src={avatar} />
+                    </ListItemAvatar>
+                    <ListItemText primary={`${name}`} secondary={affiliation} />
+                  </ListItem>
+                )
+              )}
+            </List>
+          </Card>
+        ))}
+      </Stack>
+    </Stack>
   );
 }
 
