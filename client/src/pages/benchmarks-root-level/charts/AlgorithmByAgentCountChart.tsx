7c5e37b8952abe4875404bf34289652a12f722dd
Kevin Zheng <kevin.zheng@monash.edu>
2026-03-26 07:39:01 +0000

    Better sampling of data

--- client/src/pages/benchmarks-root-level/charts/AlgorithmByAgentCountChart.tsx
+++ client/src/pages/benchmarks-root-level/charts/AlgorithmByAgentCountChart.tsx
@@ -14,6 +14,7 @@ import { formatPercentage } from "utils/format";
 import { MapPicker } from "./MapPicker";
 import { useAlgorithmChartData } from "./useAlgorithmChartData";
 import { useAlgorithmsData } from "queries/useAlgorithmQuery";
+import { sample } from "components/charts/sample";
 
 export const slices = [
   {
@@ -65,6 +66,7 @@ export function AlgorithmByAgentCountChart({
             ...keyBy(collection.data, "algorithm"),
           }))
           .sortBy("agentCount")
+          .thru(sample(500))
           .value()}
         render={
           <SliceChart
