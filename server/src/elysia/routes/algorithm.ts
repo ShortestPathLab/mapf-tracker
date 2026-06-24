ba6104e41dbcf71e11f54136dda8e7d0667173e4
Kevin Zheng <kevin.zheng@monash.edu>
2026-06-24 20:03:36 +0000

    Refactor algorithms

--- server/src/elysia/routes/algorithm.ts
+++ server/src/elysia/routes/algorithm.ts
@@ -1,22 +1,13 @@
 import { Elysia } from "elysia";
 import { Algorithm } from "models";
+import { allToJSON } from "utils/toJSON";
 
-const findAll = async (ctx: any) => {
-  return Algorithm.find({}, { _id: 1, algo_name: 1 }).then((data) => {
-    return data;
-  });
-};
 
-const findAllDetails = async (ctx: any) => {
-  return Algorithm.find({}).then((data) => {
-    return data;
-  });
-};
 
 // The bespoke best/closed/solved/lower aggregations that used to live here
 // (per map / domain / scenario / agent) are now served by the canonical
 // /api/queries/aggregate/algorithm endpoint; the client fans out one call per
 // algorithm and combines client-side. See queries.ts.
 export const algorithmRoutes = new Elysia({ prefix: "/api/algorithm" })
-  .get("/", findAll)
-  .get("/all_detail", findAllDetails);
+  .get("/", () => Algorithm.find({}, { _id: 1, algo_name: 1 }).then(allToJSON))
+  .get("/all_detail", () => Algorithm.find().then(allToJSON));
